73d2da86a7a9ed0d6640fbcf20aac1168509c023
Iwerikara <alena_gavr@bk.ru>
2021-10-15 01:08:05 +0300

    content from lesson (2)

--- Intermediate/Build/Win64/UE4Editor/Inc/Tankogeddon/Tankogeddon.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/Tankogeddon/Tankogeddon.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeTankogeddon_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x15DDDA79,
-				0x232F11D7,
+				0x4649BEA5,
+				0x8EBF8CD4,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
